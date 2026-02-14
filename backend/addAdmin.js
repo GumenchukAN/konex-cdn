790034467f9ab13fd253e4fda23e3146a17b6926
Humenchuk Anastasiia <gumenchuk.anast@gmail.com>
2026-02-14 10:22:57 +0200

    add task solution

--- backend/addAdmin.js
+++ backend/addAdmin.js
@@ -5,23 +5,17 @@ const { hash } = pkg;
 import dotenv from 'dotenv';
 dotenv.config();
 
-const client = new Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+import { pool } from './db.js';
 
-await client.connect();
+await pool.connect();
 
 const password1 = await hash('passwordFirst', 10);
 const password2 = await hash('passwordSecond', 10);
 
-await client.query(
+await pool.query(
   "INSERT INTO admin (name, password, role) VALUES ($1, $2, 'admin'), ($3, $4, 'admin')",
   ['admin1', password1, 'admin2', password2]
 );
 
-await client.end();
+await pool.end();
 console.log('Адміни додані!');
